ff5aa6cfa2ab611df8ee477521fee4903e5e32ae
huseynoff-devvo <hikmethuseynofff@gmail.com>
2025-08-27 15:46:14 +0400

    Update home.js

--- js/home.js
+++ js/home.js
@@ -980,10 +980,6 @@
                             <img src="${getProfilePic(user)}" alt="${user} profil şəkli" />
                             <span>${getUserName(user)}</span>
                         `;
-                        userDiv.onclick = () => {
-                            window.location.href = `?other_user=@${user}`;
-                            closeStoryViewer();
-                        };
                         listContainer.appendChild(userDiv);
                     });
                 } else {
